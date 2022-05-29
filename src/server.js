65f1f900a3303387b29341338e5f020dd89414c9
Joao Victor <jvmachado144@gmail.com>
2022-05-29 13:05:18 -0300

    fix: correction connection database

--- src/server.js
+++ src/server.js
@@ -15,10 +15,7 @@ app.use(express.json())
 
 app.use(Router);
 
-mongoose.connect(process.env.STRING_CONNECT_DEV_MONGO,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.STRING_CONECT_MONGO)
     .then(()=>{
       app.listen(process.env.PORT,()=>{
         console.log('server running')
