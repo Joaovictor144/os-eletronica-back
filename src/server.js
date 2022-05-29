f2f98b93fe0c0ed2c52807b47fd29ef9c139a13b
Joao Victor <jvmachado144@gmail.com>
2022-05-29 12:31:56 -0300

    feat: create dockerfile and docker-compose

--- src/server.js
+++ src/server.js
@@ -15,7 +15,10 @@ app.use(express.json())
 
 app.use(Router);
 
-mongoose.connect(process.env.STRING_CONECT_MONGO)
+mongoose.connect(process.env.STRING_CONNECT_DEV_MONGO,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
     .then(()=>{
       app.listen(process.env.PORT,()=>{
         console.log('server running')
